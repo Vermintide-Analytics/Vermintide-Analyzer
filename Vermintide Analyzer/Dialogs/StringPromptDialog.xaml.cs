78ca105254331417ffbec6128eea2f05454a4c8d
Vermintide Analytics <VermintideAnalytics@gmail.com>
2021-07-26 18:05:20 -0400

    Adding a generic dialog which takes simple string input

---
+++ Vermintide Analyzer/Dialogs/StringPromptDialog.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vermintide_Analyzer.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for StringPromptDialog.xaml
+    /// </summary>
+    public partial class StringPromptDialog : Window
+    {
+        public StringPromptDialog(string promptText, string prefillValue, string titleText = "")
+        {
+            InitializeComponent();
+            PromptText = promptText;
+            Title = titleText;
+            ResponseText = prefillValue;
+        }
+
+        public string PromptText
+        {
+            get => PromptLabel.Text;
+            set { PromptLabel.Text = value; }
+        }
+
+        public string ResponseText
+        {
+            get => ResponseTextBox.Text;
+            set { ResponseTextBox.Text = value; }
+        }
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResponseText = "";
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if(e.Key == Key.Enter)
+            {
+                DialogResult = true;
+            }
+            else if(e.Key == Key.Escape)
+            {
+                DialogResult = false;
+            }
+        }
+    }
+}

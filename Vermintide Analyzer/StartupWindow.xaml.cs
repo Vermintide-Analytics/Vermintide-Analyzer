77a7940ef23fc2fe1af01016b7ac6895bcab53d7
Vermintide Analytics <VermintideAnalytics@gmail.com>
2022-08-26 13:47:31 -0400

    Prevent most if not all startup hanging
    IT'S TIME TO STOP hanging on startup if there is an error. Now we should show an error popup and also still try to show the main window.

--- Vermintide Analyzer/StartupWindow.xaml.cs
+++ Vermintide Analyzer/StartupWindow.xaml.cs
@@ -57,53 +57,72 @@ namespace Vermintide_Analyzer
 
         private void Startup()
         {
-            BeginStep("Checking directories...");
-            if(!GameRepository.Instance.CheckDirectories())
-            {
-                BeginStep("Creating directories...");
-                GameRepository.Instance.CreateDirectories();
-            }
-
-            BeginStep("Loading user settings...");
-            Settings.Load();
-
-            BeginStep("Gathering latest console logs...");
-            GameRepository.Instance.ReadAndMoveNewGameLogs();
-
-            BeginStep("Reading old games...");
-            var existingGameHeaders = GameRepository.Instance.ReadExistingGameHeaders();
-            var previouslyInvalidGameHeaders = GameRepository.Instance.ReadPreviouslyInvalidGameHeaders();
-            LogDetail($"Found {existingGameHeaders.Count() + previouslyInvalidGameHeaders.Count()} old games.");
-
-            BeginStep("Reading new games...");
-            // Temporary bandaid for some mistakes I make in the mod output
-            GameRepository.Instance.FixNewGameData();
-            var newGameHeaders = GameRepository.Instance.ReadAndMoveNewGameHeaders();
-            LogDetail($"Found {newGameHeaders.Count()} new games.");
-
-            LogDetail($"Found {GameRepository.Instance.InvalidGames.Count} invalid games.");
-
-            BeginStep("Finishing up...");
-            GameRepository.Instance.GameHeaders.Sort((gh1, gh2) => gh2.GameStart.CompareTo(gh1.GameStart));
-            GameRepository.Instance.ReadGameNotesFromDisk();
-            GameRepository.Instance.ReadGameFiltersFromDisk();
-
-            // Delete and recreate the temp dir to clear it out
             try
             {
-                Directory.Delete(GameRepository.TempDir, true);
+                BeginStep("Checking directories...");
+                if (!GameRepository.Instance.CheckDirectories())
+                {
+                    BeginStep("Creating directories...");
+                    GameRepository.Instance.CreateDirectories();
+                }
+
+                BeginStep("Loading user settings...");
+                Settings.Load();
+
+                BeginStep("Gathering latest console logs...");
+                GameRepository.Instance.ReadAndMoveNewGameLogs();
+
+                BeginStep("Reading old games...");
+                var existingGameHeaders = GameRepository.Instance.ReadExistingGameHeaders();
+                var previouslyInvalidGameHeaders = GameRepository.Instance.ReadPreviouslyInvalidGameHeaders();
+                LogDetail($"Found {existingGameHeaders.Count() + previouslyInvalidGameHeaders.Count()} old games.");
+
+                BeginStep("Reading new games...");
+                // Temporary bandaid for some mistakes I make in the mod output
+                GameRepository.Instance.FixNewGameData();
+                var newGameHeaders = GameRepository.Instance.ReadAndMoveNewGameHeaders();
+                LogDetail($"Found {newGameHeaders.Count()} new games.");
+
+                LogDetail($"Found {GameRepository.Instance.InvalidGames.Count} invalid games.");
+
+                BeginStep("Finishing up...");
+                GameRepository.Instance.GameHeaders.Sort((gh1, gh2) => gh2.GameStart.CompareTo(gh1.GameStart));
+                GameRepository.Instance.ReadGameNotesFromDisk();
+                GameRepository.Instance.ReadGameFiltersFromDisk();
+
+                // Delete and recreate the temp dir to clear it out
+                try
+                {
+                    Directory.Delete(GameRepository.TempDir, true);
+                }
+                catch { }
+                Directory.CreateDirectory(GameRepository.TempDir);
+                GameRepository.Instance.RemoveTemporaryGameNotes();
+
+                Thread.Sleep(STARTUP_DELAY);
             }
-            catch { }
-            Directory.CreateDirectory(GameRepository.TempDir);
-            GameRepository.Instance.RemoveTemporaryGameNotes();
-
-            Thread.Sleep(STARTUP_DELAY);
-
-            Util.SafeInvoke(() =>
+            catch(Exception e)
             {
-                new MainWindow().Show();
-                Close();
-            });
+                MessageBox.Show($"{e.Message}\n{e.StackTrace}", "An error occurred during startup, the app may not function correctly", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                Util.SafeInvoke(() =>
+                {
+                    try
+                    {
+                        new MainWindow().Show();
+                    }
+                    catch
+                    {
+                        MainWindow.Instance?.Close();
+                    }
+                    finally
+                    {
+                        Close();
+                    }
+                });
+            }
         }
 
         private void BeginStep(string step)
@@ -122,7 +141,7 @@ namespace Vermintide_Analyzer
             // It became visible
             if (!HasSetup)
             {
-                new Task(Startup).Start();
+                Task.Run(Startup);
                 HasSetup = true;
             }
         }
